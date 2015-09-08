5e650fd042a127fc8d2c8289bc7eaa0415c7c410
hustzlm <zlm94smile@gmail.com>
2015-09-08 14:41:05 +0800

    up2

--- code/sortlines_2.c
+++ code/sortlines_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAXLINE 1000
 #define MAXLINES 5000
@@ -11,7 +12,7 @@ static char *allocp = allocbuf;
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void qsort(void *lineptr[], int left, int right, int (*comp) (void *, void *));
+void qsort1(void *lineptr[], int left, int right, int (*comp) (void *, void *));
 int numcmp(char *, char *);
 
 main(int argc, char *argv[])
@@ -23,7 +24,7 @@ main(int argc, char *argv[])
     numeric = 1;
   if((nlines = readlines(lineptr, MAXLINES)) >= 0)
   {
-    qsort((void **) lineptr, 0, nlines-1, (int (*) (void *, void *)) (numeric ? numcmp : strcmp));
+    qsort1((void **) lineptr, 0, nlines-1, (int (*) (void *, void *)) (numeric ? numcmp : strcmp));
     writelines(lineptr, nlines);
     return 0;
   }
@@ -81,4 +82,43 @@ void writelines(char *lineptr[], int nlines)
 {
   while(nlines-- > 0)
     printf("%s\n", *lineptr++);
+}
+
+void qsort1(void *v[], int left, int right, int (*comp) (void *, void *))
+{
+  int i, last;
+
+  void swap(void *[], int, int);
+
+  if(left >= right)
+    return;
+  swap(v, left, (left + right)/2);
+  last = left;
+  for(i=left+1; i<=right; i++)
+    if((*comp)(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  swap(v, left, last);
+  qsort1(v, left, last-1, comp);
+  qsort1(v, last+1, right, comp);
+}
+
+int numcmp(char *s1, char *s2)
+{
+  double v1, v2;
+  v1 = atof(s1);
+  v2 = atof(s2);
+  if(v1 < v2)
+    return -1;
+  else if(v1 > v2)
+    return 1;
+  else
+    return 0;
+}
+
+void swap(void *v[], int i, int j)
+{
+  void *temp;
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
 }
